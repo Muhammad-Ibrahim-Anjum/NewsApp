27ad1612f67b0bab844ef77482158a9477f85c10
Muhammad-Ibrahim-Anjum <69748200+Muhammad-Ibrahim-Anjum@users.noreply.github.com>
2021-12-20 21:31:29 +0500

    Upadted

---
+++ src/components/News.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import NewsItem from './NewsItem'
+import Alert from './Alert';
+import Spinner from './Spinner';
+
+export default class News extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+    constructor(){
+        super();
+        this.state = {
+            articles : [],
+            loading : false,
+            page : 1
+        }
+    }
+
+    async componentDidMount(){
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=fbab7de626374044b0852797eb83b459&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles:parsedData.articles, totalResults : parsedData.totalResults, loading:false});
+    }
+
+    clickPrevBtn = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=fbab7de626374044b0852797eb83b459&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles:parsedData.articles, page : this.state.page - 1, loading:false});
+    }
+    clickNextBtn = async () => {
+        if(this.state.page + 1 < Math.ceil(this.state.totalResults/21)){
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=fbab7de626374044b0852797eb83b459&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            this.setState({loading:true});
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({articles:parsedData.articles, page : this.state.page + 1, loading:false});
+        }else{
+            <Alert/>
+            console.log(<Alert/>);
+        }
+    }
+
+    render() {
+        return (
+            <div className='container my-3'>
+                <h1 className="text-center">National News Top - Headlines</h1>
+                <div className="text-center">
+                    {this.state.loading && <Spinner/>}  
+                </div>
+                <div className="row">
+                    {!this.state.loading && this.state.articles.map((element)=>{
+                        return <div className="col-md-4" key={element.url}>
+                        <NewsItem title={element.title} description = {element.description} imageUrl = {element.urlToImage} newsUrl = {element.url}/>
+                        </div>
+                    })}
+                </div>
+                <div className="flex d-flex justify-content-between">
+                    <button disabled={this.state.page<=1} type="button" onClick={this.clickPrevBtn} className="btn btn-dark">&#8592; Previous</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/21)} type="button" onClick={this.clickNextBtn} className="btn btn-dark">Next &#8594;</button>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import NewsDefaultImg from "../img/news.jpg"
+
+export default class NewsItem extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    render() {
+        let {title, description, imageUrl ,newsUrl} = this.props;
+        return (
+            <div className='my-3'>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src={imageUrl?imageUrl:NewsDefaultImg} className="card-img-top" style={{height : '150px'}} alt="NewsImage"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
