ba173c071ef3c69b05aac4c767346db8ecf2142c
Muhammad-Ibrahim-Anjum <69748200+Muhammad-Ibrahim-Anjum@users.noreply.github.com>
2021-12-21 20:26:17 +0500

    Upadted

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  //Link
 } from "react-router-dom";
 
 export default class App extends Component {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export default class NewsItem extends Component {
     }
 
     render() {
-        let {title, description, imageUrl ,newsUrl} = this.props;
+        let {title, description, imageUrl ,newsUrl ,author , publishedAt } = this.props;
         return (
             <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
@@ -16,6 +16,7 @@ export default class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-muted">By {!author?"Unknown": author} on {new Date(publishedAt).toUTCString()}</small></p>
                         <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
                     </div>
                 </div>
